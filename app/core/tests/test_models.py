1fe2ec87cf9c31ced1d9a29dd3d4fdafc32bcbf1
htpham <htpham3096@gmail.com>
2022-08-05 23:00:02 +0700

    fix linting

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -36,7 +36,7 @@ class ModelTests(TestCase):
     def test_new_user_without_email_raises_error(self):
         """Test that creating a new user without an email raises a ValueError"""
         with self.assertRaises(ValueError):
-           get_user_model().objects.create_user('', 'sample123')
+            get_user_model().objects.create_user('', 'sample123')
 
     def test_create_superuser(self):
         """Test creating a superuser"""
