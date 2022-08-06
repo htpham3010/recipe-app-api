6eb364af4fdc56405e42231343b0b5477ea0d3b5
htpham <htpham3096@gmail.com>
2022-08-06 19:26:34 +0700

    fix linting

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -65,7 +65,8 @@ class PublicUserAPITests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(email=payload["email"]).exists()
+        user_exists = get_user_model().objects.filter(
+            email=payload["email"]).exists()
 
         self.assertFalse(user_exists)
 
@@ -135,7 +136,8 @@ class PrivateUserApiTests(TestCase):
         res = self.client.get(ME_URL)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(res.data, {"name": self.user.name, "email": self.user.email})
+        self.assertEqual(
+            res.data, {"name": self.user.name, "email": self.user.email})
 
     def test_post_me_not_allowed(self):
         """Test POST is not allowed for the me endpoint"""
--- app/user/urls.py
+++ app/user/urls.py
@@ -3,11 +3,10 @@ from django.urls import path
 
 from user import views
 
-
-app_name = 'user'
+app_name = "user"
 
 urlpatterns = [
-    path('create/', views.CreateUserView.as_view(), name='create'),
-    path('token/', views.CreateTokenView.as_view(), name='token'),
-    path('me/', views.ManageUserView.as_view(), name='me'),
+    path("create/", views.CreateUserView.as_view(), name="create"),
+    path("token/", views.CreateTokenView.as_view(), name="token"),
+    path("me/", views.ManageUserView.as_view(), name="me"),
 ]
